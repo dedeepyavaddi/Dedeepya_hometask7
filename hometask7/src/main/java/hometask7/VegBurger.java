60c8ad989ff4662fa90981da5a358f0e750b5b41
dedeepyavaddi <60175433+dedeepyavaddi@users.noreply.github.com>
2020-03-09 23:40:03 +0530

    Add files via upload

---
+++ hometask7/src/main/java/hometask7/VegBurger.java
@@ -0,0 +1,12 @@
+package hometask7;
+
+public class VegBurger extends Burger {
+	   public float price() {
+		      return 25.0f;
+		   }
+
+		   public String name() {
+		      return "Veg Burger";
+		   }
+
+}
---
+++ hometask7/src/main/java/hometask7/Wrapper.java
@@ -0,0 +1,9 @@
+package hometask7;
+
+public class Wrapper implements Packing {
+	  
+	   public String pack() {
+	      return "Wrapper";
+	   }
+
+}
